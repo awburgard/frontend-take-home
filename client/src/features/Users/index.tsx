acb25696c3c1b450ce0a0fc2327cb765983bca86
Austin Burgard <awburagrd@gmail.com>
2024-12-04 10:25:56 -0600

    stubs table for roles and users

--- client/src/features/Users/index.tsx
+++ client/src/features/Users/index.tsx
@@ -1,3 +1,13 @@
+import { Table } from '../../components/Table'
+
 export default function Users() {
-  return <div>Users</div>
+  return (
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.Cell>Name</Table.Cell>
+        </Table.Row>
+      </Table.Header>
+    </Table>
+  )
 }
