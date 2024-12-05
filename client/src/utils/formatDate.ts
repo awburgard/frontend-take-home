f22eb957af305c361134d48cc61f14ee181a3ec3
Austin Burgard <awburagrd@gmail.com>
2024-12-05 13:12:16 -0600

    adds default return for format date

--- client/src/utils/formatDate.ts
+++ client/src/utils/formatDate.ts
@@ -1,6 +1,10 @@
 export function formatDate(dateString: string): string {
   const date = new Date(dateString)
-  
+
+  if (isNaN(date.getTime())) {
+    return 'Invalid Date'
+  }
+
   return date.toLocaleDateString('en-US', {
     month: 'short',
     day: 'numeric',
