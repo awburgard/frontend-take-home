3842ca4f62492f49b5d0e8554d78177531128bde
Austin Burgard <awburagrd@gmail.com>
2024-12-05 11:46:48 -0600

    updates role query for role cell

--- client/src/features/Users/RoleCell.tsx
+++ client/src/features/Users/RoleCell.tsx
@@ -1,7 +1,11 @@
 import { Table } from '../../components/Table'
-import { useRoleQuery } from '../Roles/queries'
+import { useRolesQuery } from '../Roles/queries'
 
 export default function RoleCell({ id }: { id: string }) {
-  const { data } = useRoleQuery(id)
-  return <Table.Cell>{data?.name}</Table.Cell>
+  const { data } = useRolesQuery({ page: 1, search: '' }) // this could be done better to avoid extra query or fetching all possible roles
+  const role = data?.data.find((role) => role.id === id)
+
+  if (!role) return <Table.Cell>Unknown role</Table.Cell>
+
+  return <Table.Cell>{role?.name}</Table.Cell>
 }
