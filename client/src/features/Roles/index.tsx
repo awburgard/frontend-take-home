a7b62af671d5ac55fb3f20c8e1f57d0aa22b6ac9
Austin Burgard <awburagrd@gmail.com>
2024-12-04 21:56:07 -0600

    inits roles table

--- client/src/features/Roles/index.tsx
+++ client/src/features/Roles/index.tsx
@@ -1,13 +1,85 @@
+import { useCallback, useState } from 'react'
 import { Table } from '../../components/Table'
+import { Search } from '../Search'
+import { Avatar, Button, DropdownMenu, Flex, Spinner } from '@radix-ui/themes'
+import { useDebounce } from '../../hooks/useDebouce'
+import { useRolesQuery } from './queries'
+import { formatDate } from '../../utils/formatDate'
 
 export default function Roles() {
+  const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+  const debouncedSearchTerm = useDebounce(search, 300)
+  const { data, isLoading } = useRolesQuery({
+    page,
+    search: debouncedSearchTerm,
+  })
+
+  const handleSearchChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value)
+    },
+    [search]
+  )
+
   return (
-    <Table variant='surface'>
-      <Table.Header>
-        <Table.Row>
-          <Table.Cell>Name</Table.Cell>
-        </Table.Row>
-      </Table.Header>
-    </Table>
+    <>
+      <Search
+        placeholder='Search by role...'
+        search={search}
+        onChange={handleSearchChange}
+      />
+      <Table variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Default</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {!isLoading ? (
+            <>
+              {data?.data.map((role) => {
+                return (
+                  <Table.Row key={role.id}>
+                    <Table.RowHeaderCell>{role.name}</Table.RowHeaderCell>
+                    <Table.Cell>{role.description}</Table.Cell>
+                    <Table.Cell>{role.isDefault ? 'Yes' : 'No'}</Table.Cell>
+                    <Table.Cell>{formatDate(role.createdAt)}</Table.Cell>
+                    <Table.Cell>
+                      {/* <ActionMenu roleId={role.id} name={role.name} /> */}
+                    </Table.Cell>
+                  </Table.Row>
+                )
+              })}
+            </>
+          ) : (
+            <Spinner loading={isLoading} />
+          )}
+        </Table.Body>
+
+        {Boolean(data?.next || data?.prev) && (
+          <Flex justify='between' py='2'>
+            <Button
+              variant='soft'
+              onClick={() => setPage(data?.prev || 1)}
+              disabled={!data?.prev}
+            >
+              Previous
+            </Button>
+            <Button
+              variant='soft'
+              onClick={() => setPage(data?.next || 1)}
+              disabled={!data?.next}
+            >
+              Next
+            </Button>
+          </Flex>
+        )}
+      </Table>
+    </>
   )
 }
