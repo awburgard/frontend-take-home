83cb6302c19f5c1c1412d3faa78d2575ab2ac294
Austin Burgard <awburagrd@gmail.com>
2024-12-09 08:59:28 -0600

    removes unneeded forward ref and imperative handle

--- client/src/features/Users/ActionMenu.tsx
+++ client/src/features/Users/ActionMenu.tsx
@@ -1,6 +1,6 @@
 import { DotsHorizontalIcon } from '@radix-ui/react-icons'
 import { DropdownMenu, IconButton } from '@radix-ui/themes'
-import React, { forwardRef, useImperativeHandle, useState } from 'react'
+import React, { useState } from 'react'
 
 import { DeleteUserDialog } from '@/features/Users/DeleteUserDialog'
 import { EditUserDialog } from '@/features/Users/EditUserDialog'
@@ -11,45 +11,38 @@ interface ActionMenuProps {
   render: (toggleDialog: (dialogType: string) => void) => React.ReactNode
 }
 
-export const ActionMenu = forwardRef(
-  ({ user, render }: ActionMenuProps, ref) => {
-    const [dialogType, setDialogType] = useState<string | null>(null)
+export const ActionMenu = ({ user, render }: ActionMenuProps) => {
+  const [dialogType, setDialogType] = useState<string | null>(null)
 
-    useImperativeHandle(ref, () => ({
-      openDialog: (type: string) => setDialogType(type),
-      closeDialog: () => setDialogType(null),
-    }))
+  return (
+    <>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <IconButton
+            variant='ghost'
+            color='gray'
+            radius='full'
+            aria-label='Actions'
+          >
+            <DotsHorizontalIcon width='16px' height='16px' />
+          </IconButton>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content sideOffset={5}>
+          {render((type) => setDialogType(type))}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
 
-    return (
-      <>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger>
-            <IconButton
-              variant='ghost'
-              color='gray'
-              radius='full'
-              aria-label='Actions'
-            >
-              <DotsHorizontalIcon width='16px' height='16px' />
-            </IconButton>
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content sideOffset={5}>
-            {render((type) => setDialogType(type))}
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
+      {dialogType === 'delete' && (
+        <DeleteUserDialog
+          name={`${user.first} ${user.last}`}
+          userId={user.id}
+          setDialogType={setDialogType}
+        />
+      )}
 
-        {dialogType === 'delete' && (
-          <DeleteUserDialog
-            name={`${user.first} ${user.last}`}
-            userId={user.id}
-            setDialogType={setDialogType}
-          />
-        )}
-
-        {dialogType === 'edit' && (
-          <EditUserDialog user={user} setDialogType={setDialogType} />
-        )}
-      </>
-    )
-  }
-)
+      {dialogType === 'edit' && (
+        <EditUserDialog user={user} setDialogType={setDialogType} />
+      )}
+    </>
+  )
+}
