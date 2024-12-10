00b7c10a29401e81e9593dc8005fc40dbfb7e679
Austin Burgard <awburagrd@gmail.com>
2024-12-10 11:41:17 -0600

    updates footer

--- client/src/components/Table/Footer.tsx
+++ client/src/components/Table/Footer.tsx
@@ -1,15 +1,45 @@
+import './table.footer.css'
+
+import { Table as RadixTable } from '@radix-ui/themes'
+import { useMemo } from 'react'
+
 import { Table } from '@/components/Table'
 
-interface TableFooterProps extends React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode
+type Align = RadixTable.CellProps['justify']
+
+interface TableFooterProps
+  extends React.HTMLAttributes<HTMLTableSectionElement> {
+  children?: React.ReactNode
+  align?: Align
+  colSpan?: number
 }
 
-export const TableFooter = ({ children }: TableFooterProps) => (
-  <tfoot style={{ borderTop: '1px solid var(--gray-a5)' }}>
-    <Table.Row>
-      <Table.Cell colSpan={999} align='right'>
-        {children}
-      </Table.Cell>
-    </Table.Row>
-  </tfoot>
-)
+export const TableFooter: React.FC<TableFooterProps> = ({
+  children,
+  colSpan = 999,
+  align = 'end',
+  ...props
+}) => {
+  const renderRow = useMemo(
+    () => (content: React.ReactNode, align: Align) =>
+      (
+        <Table.Row>
+          <Table.Cell
+            colSpan={colSpan}
+            justify={align}
+            className='rt-TableFooterRow'
+            data-justify={align}
+          >
+            {content}
+          </Table.Cell>
+        </Table.Row>
+      ),
+    [colSpan]
+  )
+
+  return (
+    <tfoot className='rt-TableFooter' {...props}>
+      {children && renderRow(children, align)}
+    </tfoot>
+  )
+}
